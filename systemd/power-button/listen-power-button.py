4cfe05e77b47177646fb996d2a73255b56477b00
Peter Bestler <pbestler@riseup.net>
2025-03-23 21:08:02 +0100

    Fix indention running black.

--- systemd/power-button/listen-power-button.py
+++ systemd/power-button/listen-power-button.py
@@ -4,9 +4,8 @@ import RPi.GPIO as GPIO
 import logging
 
 
-def change_led(led_name="PWR", state):
+def change_led(led_name="PWR", state="on"):
     """Turns the Raspberry Pi LED ON or OFF based on the given state.
-
     Args:
         led_name (str): The name of the LED PWR or ACT (default: "PWR" for Raspberry Pi).
         state (str): "on" to turn the LED on, "off" to turn it off.
@@ -33,21 +32,19 @@ def change_led(led_name="PWR", state):
     except PermissionError:
         print("Permission denied! Try running with sudo.")
     except FileNotFoundError:
-        print(
-            f"LED {led_name} not found. Check available LEDs in /sys/class/leds/")
+        print(f"LED {led_name} not found. Check available LEDs in /sys/class/leds/")
     except Exception as e:
         print(f"Error: {e}")
 
 
 def wait_for_powerbutton(gpio_id=3):
-
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio_id, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     print(f"Registered GPIO{gpioid} as shutdown button. Waiting. ")
     try:
         GPIO.wait_for_edge(gpio_id, GPIO.FALLING)
         print(f"Shutdown triggered by GPIO{gpio_id} -> Shutting down.")
-        subprocess.call(['shutdown', '-h', 'now'], shell=False)
+        subprocess.call(["shutdown", "-h", "now"], shell=False)
     except Exception as e:
         print(f"Error during shutdown.")
     finally:
